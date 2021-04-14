4ce07af7da02729767a59d8dc3cf8097c5d42b57
Stevetoretto <stephenjoseph1111@gmail.com>
2021-04-14 17:16:04 +0530

    updated git

--- src/test/java/org/sample/GitSample.java
+++ src/test/java/org/sample/GitSample.java
@@ -3,6 +3,8 @@ package org.sample;
 public class GitSample {
 	public static void main(String[] args) {
 		System.out.println("new code");
+		System.out.println("modified");
+		System.out.println("steve");
 	}
 
 }
